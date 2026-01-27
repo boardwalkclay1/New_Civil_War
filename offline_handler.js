4ab0a20d3155a8f39aae94ef28a3cabb26a334ef
boardwalkclay1 <boardwalkclay1@gmail.com>
2026-01-27 11:53:48 -0500

    Create offline_handler.js

---
+++ offline_handler.js
@@ -0,0 +1,9 @@
+/* OFFLINE HANDLER */
+
+window.addEventListener("offline", () => {
+  console.log("You are offline.");
+});
+
+window.addEventListener("online", () => {
+  console.log("You are back online.");
+});
